0c4847f32afe832bf389a04c7a91efad959a39bb
silence592590119 <821445893@qq.com>
2019-10-09 16:46:09 +0800

    更换IP

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export default {
     "baseURL" : "http://123.56.226.102:8056/api",
-    "productURL":"http://219.135.135.151:3001/api",
+    "productURL":"http://192.168.170.205:3001/api",
     "xianURL":"http://39.105.20.125:3001/api",
     "gongURL":"http://47.93.76.33:8008/api"
 }
